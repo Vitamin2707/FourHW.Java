5e8e11c7d427b5701644896cd7ef4e0dcc752305
Artur <artur.gikramin@yandex.ru>
2022-11-15 22:03:24 +0500

    -A

---
+++ src/onehw/test.java
@@ -0,0 +1,14 @@
+package onehw;
+
+public class test{
+
+    public static void main(String[] args) {
+        int A=(-8/-4);
+        int B=(100%3);
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(A+B);
+   
+    }
+    
+}
